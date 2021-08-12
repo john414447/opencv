4fe591134088b3a8b6f8e4f69c88b5cf7899c9cf
John <q74663214@mailst.cjcu.edu.tw>
2021-08-12 17:38:32 +0800

    15

---
+++ 15-3.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+img = cv2.imread('2.png',0)
+# global thresholding
+ret1,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+# Otsu's thresholding
+ret2,th2 = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+# Otsu's thresholding after Gaussian filtering
+#虹5,5虺为咇斯核的大小虽0 为标准差
+blur = cv2.GaussianBlur(img,(5,5),0)
+# 侷值一定䌰䕭为 0虲
+ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+# plot all the images and their histograms
+images = [img, 0, th1,
+    img, 0, th2,
+    blur, 0, th3]
+titles = ['Original Noisy Image','Histogram','Global Thresholding (v=127)',
+'Original Noisy Image','Histogram',"Otsu's Thresholding",
+'Gaussian filtered Image','Histogram',"Otsu's Thresholding"]
+# 䦈䭻使用了 pyplot 中画直方图的方法虽plt.hist, 䌰注意的是它的参数是一维数组
+# 所以䦈䭻使用了虹numpy虺ravel 方法虽将多维数组䤛换成一维虽也可以使用 flatten 方法
+#ndarray.flat 1-D iterator over an array.
+#ndarray.flatten 1-D array copy of the elements of an array in row-major order.
+for i in xrange(3):
+    plt.subplot(3,3,i*3+1),plt.imshow(images[i*3],'gray')
+    plt.title(titles[i*3]), plt.xticks([]), plt.yticks([])
+    plt.subplot(3,3,i*3+2),plt.hist(images[i*3].ravel(),256)
+    plt.title(titles[i*3+1]), plt.xticks([]), plt.yticks([])
+    plt.subplot(3,3,i*3+3),plt.imshow(images[i*3+2],'gray')
+    plt.title(titles[i*3+2]), plt.xticks([]), plt.yticks([])
+    plt.show()
